8b3649f0dfb0854713824e19a972027d6900dd05
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-09 12:54:15 -0700

    Imported/Exported: filterObj and sanitize utility functions

--- utils/index.js
+++ utils/index.js
@@ -2,5 +2,14 @@ const AppError = require('./appError');
 const capitalize = require('./capitalize');
 const catchAsync = require('./catchAsync');
 const Email = require('./email');
+const filterObj = require('./filterObject');
+const sanitize = require('./sanitize');
 
-module.exports = { AppError, capitalize, catchAsync, Email };
+module.exports = {
+  AppError,
+  capitalize,
+  catchAsync,
+  Email,
+  filterObj,
+  sanitize,
+};
