f7fed19b97c01815038d204c4766fcda3450ad2a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-09 12:18:19 -0700

    Imported/Exported: capitalize function

--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
 const AppError = require('./appError');
+const capitalize = require('./capitalize');
 const catchAsync = require('./catchAsync');
 const Email = require('./email');
 
-module.exports = { AppError, catchAsync, Email };
+module.exports = { AppError, capitalize, catchAsync, Email };
