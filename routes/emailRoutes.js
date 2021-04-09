f6132a3a89cea540adaeca078f55fc18ebee7ec2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-09 12:44:57 -0700

    Imported: validationController middleware. Added: validationController middleware to the /sendEmail post route

--- routes/emailRoutes.js
+++ routes/emailRoutes.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const { emailController } = require('../controllers');
 
+const { validationController } = require('../validations');
+
 const router = express.Router();
 
-router.post('/sendEmail', emailController.sendEmail);
+router.post(
+  '/sendEmail',
+  validationController.sendEmail,
+  emailController.sendEmail
+);
 
 module.exports = router;
