e90716933106bba0d56b4ba657b9a0307a259d9f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-04-08 22:16:58 -0700

    Imported Dependencies: html-to-text, nodemailer, and pug. Created/Exported: a class for sending out an email with a transport, mailtrap, sendgrid, and pug

---
+++ utils/email.js
@@ -0,0 +1,64 @@
+const { htmlToText } = require('html-to-text');
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+
+module.exports = class Email {
+  constructor(name, email, message, website, email_to) {
+    this.to = email_to;
+    this.name = name;
+    this.email = email;
+    this.message = message;
+    this.website = website;
+    this.from = `Elias Gutierrez, Contact Form API <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Use Sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+
+    // Use Mailtrap
+    return nodemail.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      name: this.name,
+      email: this.email,
+      message: this.message,
+      website: this.website,
+      subject,
+    });
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(text),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendMessage() {
+    await this.send(
+      'contact',
+      '[Contact Form API] A new message has been sent to you'
+    );
+  }
+};
